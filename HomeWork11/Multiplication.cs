98928a15d4f0dde6ccb960372db574e93b9f53c6
Maksentiy <maksim.konon.96@gmail.com>
2025-06-18 13:19:48 +0300

    Solution for home work 11

---
+++ HomeWork11/Multiplication.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork11;
+
+public class Multiplication
+{
+    public static int MultiplyByFive(string num1) 
+    {
+        if (num1 == null) 
+        {
+            throw new Exception("num1 = null");
+        }
+        if (num1 == "") 
+        {
+            throw new Exception("string is empty");
+        }
+        if (!Int32.TryParse(num1, out int result)) 
+        {
+            throw new Exception("Incorrect input");
+        }
+
+
+        return result * 5; 
+    }
+}
---
+++ HomeWork11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWork11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string nullString = null;
+        string emptyString = "";
+        string incorrectInput = "sd64s6df ";
+        string num1 = "89";
+
+        Check(nullString);
+        Check(emptyString);
+        Check(incorrectInput);
+        Check(num1);
+    }
+
+    public static void Check(string num) 
+    {
+        try
+        {
+            var result = Multiplication.MultiplyByFive(num);
+            Console.WriteLine("result = " + result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
